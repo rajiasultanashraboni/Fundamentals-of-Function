ce99364c737b7ac52cca7c61fd24c7244ed2ddc4
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-21 18:39:27 +0600

    conditional return is here

---
+++ conditional_return.js
@@ -0,0 +1,19 @@
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isEven(20));
+
+function odd(number) {
+  if (number % 2 === 1) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(odd(5));
--- return.js
+++ return.js
@@ -12,3 +12,13 @@ function cutHalf(number) {
 
 const output = cutHalf(10);
 console.log(output);
+
+function doMath(num1, num2) {
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+  const multiply = sum * diff;
+  const result = multiply / 2;
+  return result;
+}
+
+console.log(doMath(10, 5));
