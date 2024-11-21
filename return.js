237a28949e5eceb138c9506623751360f401aa57
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-21 18:16:27 +0600

    add example functions for multiplying and dividing numbers

---
+++ return.js
@@ -0,0 +1,14 @@
+function tenTimes(number) {
+  const result = number * 10;
+  return result;
+}
+
+tenTimes(10);
+
+function cutHalf(number) {
+  const result = number / 2;
+  return result;
+}
+
+const output = cutHalf(10);
+console.log(output);
