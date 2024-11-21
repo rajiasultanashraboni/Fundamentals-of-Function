bb3e55b322eee0fce69a6390a4905d5e54b7f791
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-21 11:23:51 +0600

    number of borgo is calculated here using function method

--- parameter.js
+++ parameter.js
@@ -6,3 +6,15 @@ function add(num1, num2) {
 
 // console.log("add number:", sum);
 add(4, 5);
+
+function square(number) {
+  const borgo = number * number;
+  console.log("number of borgo:", borgo);
+}
+
+square(5);
+square(2);
+square(3);
+square(15);
+square(12);
+square(14);
