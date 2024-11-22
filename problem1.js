15ac9c38b1a1e6201bfe67cf736ef1904ac445a9
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-22 13:57:48 +0600

    convert mile to kilo and inch to feet

---
+++ problem1.js
@@ -0,0 +1,14 @@
+function inchToFeet(inch) {
+  const feet = inch / 12;
+  return feet;
+}
+
+// const result = inchToFeet(75);
+// console.log(result);
+
+function mileToKilometer(mile) {
+  const kilo = mile * 1.609;
+  return kilo;
+}
+const result = mileToKilometer(50);
+console.log(result);
