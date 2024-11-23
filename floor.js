09ee0fa34feea10037bddfe9925bc25620759c62
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-23 10:45:15 +0600

    javascript floor method is here

---
+++ floor.js
@@ -0,0 +1,6 @@
+console.log(Math.floor(2.25));
+console.log(Math.floor(2.55));
+console.log(Math.floor(3.85));
+console.log(Math.floor(5.99));
+
+// javascript er floor method holo bangalir moto jara onner upore utha dekhte pare na thik temni floor o kaoke upore uthte dei na tene niche namiye dei....
