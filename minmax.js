ae25b90b65124de7cb6b15a3c29332849e7bb42c
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-23 10:39:36 +0600

    how to get PI value and how abs method is works has been implemented here

--- minmax.js
+++ minmax.js
@@ -3,3 +3,9 @@ console.log(min);
 
 const max = Math.max(2, 4, 5, 6, 7, 8, 9, 10, 15);
 console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.abs(88));
+console.log(Math.abs(10 - 5)); //
+console.log(Math.abs(5 - 10));
