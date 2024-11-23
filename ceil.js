013f1641aacff5cb4c546b602d235b6b41c7e2b6
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-23 10:48:10 +0600

    ceil method is here

---
+++ ceil.js
@@ -0,0 +1,5 @@
+console.log(Math.ceil(3.25));
+console.log(Math.ceil(3.85));
+console.log(Math.ceil(3.2));
+
+//ceil hocche kono floating number ke upore uthai dei and ekta integer shongkha return kore
